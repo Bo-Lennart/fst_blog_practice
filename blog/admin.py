5c028f5478a318c4feb5add801c38864d34b9128
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-11-30 14:51:44 +0000

    add post admin searchfield

--- blog/admin.py
+++ blog/admin.py
@@ -7,6 +7,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
-    summernote_fields = ('content')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
