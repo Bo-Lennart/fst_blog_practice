75dba32c4fea1940db4e38e975000a8d848ae97e
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-11-30 15:00:03 +0000

    add post and comment administration

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -12,3 +12,14 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    action = ['approve_comments']
+
+    def approve_comments(self, request, quesryset):
+        queryset.update(approved=True)
